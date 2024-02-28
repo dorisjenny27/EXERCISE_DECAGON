8c816943a54a2ad17219003a0e44b0af6ccdb437
dorisjenny27 <dorisjenny27@gmail.com>
2024-02-28 13:20:38 +0100

    week 2

---
+++ WEEK 2 - CLASS EXERCISE/WEEK 2 - CLASS EXERCISE/DelegateCalculatorExample.cs
@@ -0,0 +1,19 @@
+﻿namespace WEEK_2___CLASS_EXERCISE
+{
+    internal class DelegateCalculatorExample
+    {
+        static void Main(string[] args)
+        {
+            AddNumbers((a, b) => a + b);
+        }
+        static void AddNumbers(Func<int, int, double> sum)
+        {
+            int x = 6;
+            int y = 7;
+            double result = sum(x, y);
+
+            Console.WriteLine("Result of addition" + result);
+        }
+
+    }
+}
